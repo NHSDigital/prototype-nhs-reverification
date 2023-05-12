17b658003216a7c55f2a602dd64ddd76a8727c25
Matt Fielding <matt@hippodigital.co.uk>
2023-05-12 17:43:47 +0100

    add modal code

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,1 +1,84 @@
 // ES6 or Vanilla JavaScript
+
+// modal window
+(function() {
+
+  'use strict'
+
+  // list out the vars
+  var mOverlay = getId('modal_window'),
+    mOpen = getId('modal_open'),
+    mCreate = getId('modal_create'),
+    mClose = getId('modal_close'),
+    modal = getId('modal_holder'),
+    emailField = getId('emailAddress'),
+    allNodes = document.querySelectorAll("*"),
+    modalOpen = false,
+    lastFocus,
+    i
+
+  // wrap all this and check the modal is on the page first
+  if (mOverlay !== null) {
+    // Let's open the modal
+    function modalShow ( event ) {
+      event.preventDefault ? event.preventDefault() : event.returnValue = false
+      lastFocus = document.activeElement
+      mOverlay.setAttribute('aria-hidden', 'false')
+      modalOpen = true
+      modal.setAttribute('tabindex', '0')
+      modal.forms[0].elements[0].focus()
+      modal.focus()
+      mOverlay.scrollTop(0)
+      emailField.focus()
+    }
+
+    // binds to both the button click and the escape key to close the modal window
+    // but only if modalOpen is set to true
+    function modalClose ( event ) {
+      if (modalOpen && ( !event.keyCode || event.keyCode === 27 ) ) {
+        mOverlay.setAttribute('aria-hidden', 'true')
+        modal.setAttribute('tabindex', '-1')
+        event.preventDefault()
+        modalOpen = false
+        lastFocus.focus()
+      }
+    }
+
+    // Restrict focus to the modal window when it's open.
+    // Tabbing will just loop through the whole modal.
+    // Shift + Tab will allow backup to the top of the modal,
+    // and then stop.
+    function focusRestrict ( event ) {
+      if (modalOpen && !modal.contains( event.target ) ) {
+        event.stopPropagation()
+        modal.focus()
+      }
+    }
+
+    // Close modal window by clicking on the overlay
+    mOverlay.addEventListener('click', function( e ) {
+      if (e.target == modal.parentNode) {
+        modalClose( e )
+      }
+    }, false)
+
+    // open modal by btn click/hit
+    // mOpen.addEventListener('click', modalShow)
+    mCreate.addEventListener('click', modalShow, false)
+    // close modal by btn click/hit
+    mClose.addEventListener('click', modalClose)
+
+    // close modal by keydown, but only if modal is open
+    document.addEventListener('keydown', modalClose)
+
+    // restrict tab focus on elements only inside modal window
+    for (i = 0; i < allNodes.length; i++) {
+      allNodes.item(i).addEventListener('focus', focusRestrict)
+    }
+  }
+
+  // Let's cut down on what we need to type to get an ID
+  function getId ( id ) {
+    return document.getElementById(id)
+  }
+})()
